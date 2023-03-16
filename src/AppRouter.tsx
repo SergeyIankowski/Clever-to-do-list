a53f5e4066ccb81fcc97aa88a10d8092d1fa5bf3
SeregeyIankowski <garret933@gmail.com>
2023-03-16 10:43:19 +0300

    feat: add AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Page404 from "./components/Page404/Page404";
+import Register from "./components/Register/Register";
+import TodoBoard from "./components/todoBoard/TodoBoard";
 import Pages from "./models/Pages";
-import Main from "./pages/Main";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path={Pages.notfound} element={<Main />} />
-      <Route path={Pages.main} element={<Main />} />
+      <Route path={Pages.notfound} element={<Page404 />} />
+      <Route path={Pages.login} element={<Login />} />
+      <Route path={Pages.register} element={<Register />} />
+      <Route path={Pages.todoBoard} element={<TodoBoard />} />
     </Routes>
   );
 };
