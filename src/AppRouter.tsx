ae8df924b5caaa9283ee4698bd28f07b4d965a20
SeregeyIankowski <garret933@gmail.com>
2023-03-23 15:54:47 +0300

    feat: add redirecting when user is not authorized

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
@@ -10,28 +10,36 @@ import ContextType from "./models/Context.type";
 import FirebaseContext from "./models/firebaseContext";
 import Pages from "./models/Pages";
 import ConcreteTodo from "./pages/ConcreteTodo/ConcreteTodo";
+import fetchUserName from "./firebase/fetchUserName";
+import Spinner from "./components/Spinner/Spinner";
 
 const AppRouter = () => {
   const { auth } = useContext(FirebaseContext) as ContextType;
-  const [user] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      const serverRequestDelay = 1000;
+      setTimeout(() => fetchUserName(user, setName), serverRequestDelay);
+    }
+  }, [loading]);
+
+  if (loading) return <Spinner />;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <Routes>
-      <Route path={Pages.main} element={user ? <TodoBoard /> : <Login />} />
-      <Route path={Pages.notfound} element={<Page404 />} />
-      <Route path={Pages.login} element={<Login />} />
-      <Route path={Pages.register} element={<Register />} />
-      <Route path={Pages.reset} element={<ResetPassword />} />
-      <Route path={Pages.todoBoard} element={<TodoBoard />} />
       <Route
-        path={Pages.todoPage}
-        element={
-          <ConcreteTodo
-            title="Todo title"
-            description="Some text"
-            done={false}
-          />
-        }
+        path={Pages.main}
+        element={user ? <TodoBoard user={user!} name={name} /> : <Login user={user!} />}
       />
+      <Route path={Pages.notfound} element={<Page404 />} />
+      <Route path={Pages.login} element={<Login user={user!} />} />
+      <Route path={Pages.todoBoard} element={<TodoBoard user={user!} name={name} />} />
+      <Route path={Pages.todoPage} element={<ConcreteTodo user={user!} />} />
+      <Route path={Pages.register} element={<Register user={user!} />} />
+      <Route path={Pages.reset} element={<ResetPassword user={user!} />} />
     </Routes>
   );
 };
